2f19aa5e05052bb3940806288acdeda7718b495f
JPEtienne <ji.pe.7@hotmail.com>
2021-11-23 15:33:13 +0100

    Working on services

---
+++ src/student/student.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common'
+import { students } from 'src/db'
+@Injectable()
+export class StudentService {
+  private students = students
+
+  getStudents() {
+    return this.students
+  }
+}
